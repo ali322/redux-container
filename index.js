8f9c1a5d96e558729b1aa4f8c93efb66634c2d62
alichen <ali322@gmail.com>
2017-02-05 11:35:40 +0800

    update v1.0.5

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import createLogger from "redux-logger"
 
 let middlewares = [thunkMiddleware]
 
-const isDev = typeof __DEV__ !== 'undefined' && __DEV__
+const isDev = (typeof __DEV__ !== 'undefined' && __DEV__) || process.env.NODE_ENV !== 'production'
 
 if(isDev && typeof window !== "undefined"){
     middlewares.push(createLogger())
@@ -14,22 +14,35 @@ if(isDev && typeof window !== "undefined"){
 
 const configureStore = compose(applyMiddleware(...middlewares))(createStore)
 
-export default function (rootReducer,initialState={},actions={},mapStateToProps=state=>state){
+export function connected(actions={},mapStateToProps=state=>state){
     return OriginalComponent=>{
-        const store = configureStore(rootReducer,initialState)
         const mapActionToProps = dispatch=>({
             dispatch,
             actions:bindActionCreators(actions,dispatch)
         })
-        const ConnectedComponent = connect(mapStateToProps,mapActionToProps)(OriginalComponent)
+        return connect(mapStateToProps,mapActionToProps)(OriginalComponent)
+    }
+}
+
+export function wrapper(rootReducer,initialState){
+    return OriginalComponent=>{
+        const store = configureStore(rootReducer,initialState)
         return class extends Component{
             render(){
                 return (
                     <Provider store={store}>
-                    <ConnectedComponent {...this.props}/>
+                    <OriginalComponent {...this.props}/>
                     </Provider>
                 )
             }
         }
+    }
+}
+
+export default function (rootReducer,initialState={},actions={},mapStateToProps=state=>state){
+    return OriginalComponent=>{
+        const store = configureStore(rootReducer,initialState)
+        const ConnectedComponent = connected(actions,mapStateToProps)(OriginalComponent)
+        return wrapper(rootReducer,initialState)(ConnectedComponent)
     }
 }
