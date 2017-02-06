a1ddb8851fc24ca228358d5fcea14c69998de3a8
alichen <ali322@gmail.com>
2017-02-06 16:37:10 +0800

    update v0.0.14

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import {Provider,connect} from "react-redux"
-import React,{Component} from "react"
-import {createStore,applyMiddleware,compose,bindActionCreators} from "redux"
+import { Provider, connect } from "react-redux"
+import React, { Component } from "react"
+import { createStore, applyMiddleware, compose, bindActionCreators } from "redux"
 import thunkMiddleware from "redux-thunk"
 import createLogger from "redux-logger"
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 let middlewares = [thunkMiddleware]
 
-const isDev = (typeof __DEV__ !== 'undefined' && __DEV__) || process.env.NODE_ENV !== 'production'
+const isDev = (typeof __DEV__ !== 'undefined' && __DEV__) ||
+    (process.env.NODE_ENV !== 'production' && typeof window !== "undefined")
 
-if(isDev && typeof window !== "undefined"){
+if (isDev) {
     middlewares.push(createLogger())
 }
 
-export const configureStore = compose(applyMiddleware(...middlewares))(createStore)
+export const configureStore = compose(isDev ? composeWithDevTools(applyMiddleware(...middlewares)) :
+    applyMiddleware(...middlewares))(createStore)
 
-export function connected(actions={},mapStateToProps=state=>state){
-    return OriginalComponent=>{
-        const mapActionToProps = dispatch=>({
+export function connected(actions = {}, mapStateToProps = state => state) {
+    return OriginalComponent => {
+        const mapActionToProps = dispatch => ({
             dispatch,
-            actions:bindActionCreators(actions,dispatch)
+            actions: bindActionCreators(actions, dispatch)
         })
-        return connect(mapStateToProps,mapActionToProps)(OriginalComponent)
+        return connect(mapStateToProps, mapActionToProps)(OriginalComponent)
     }
 }
 
-export function wrapper(store){
-    return OriginalComponent=>{
-        return class extends Component{
-            render(){
+export function wrapper(store) {
+    return OriginalComponent => {
+        return class extends Component {
+            render() {
                 return (
                     <Provider store={store}>
                     <OriginalComponent {...this.props}/>
@@ -38,10 +41,10 @@ export function wrapper(store){
     }
 }
 
-export default function (rootReducer,initialState={},actions={},mapStateToProps=state=>state){
-    return OriginalComponent=>{
-        const store = configureStore(rootReducer,initialState)
-        const ConnectedComponent = connected(actions,mapStateToProps)(OriginalComponent)
+export default function(rootReducer, initialState = {}, actions = {}, mapStateToProps = state => state) {
+    return OriginalComponent => {
+        const store = configureStore(rootReducer, initialState)
+        const ConnectedComponent = connected(actions, mapStateToProps)(OriginalComponent)
         return wrapper(store)(ConnectedComponent)
     }
 }
