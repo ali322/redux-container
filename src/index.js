1d7ed80a2fb9642ece38119e3854055d47182e75
alichen <ali322@gmail.com>
2017-03-31 10:28:39 +0800

    update v1.0.23

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Provider, connect } from "react-redux"
 import React from "react"
 import { createStore, applyMiddleware, compose, bindActionCreators } from "redux"
 import thunkMiddleware from "redux-thunk"
+import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'remote-redux-devtools'
 
 let middlewares = [thunkMiddleware]
@@ -9,34 +10,37 @@ let middlewares = [thunkMiddleware]
 const isDev = (typeof __DEV__ !== 'undefined' && __DEV__) ||
     (process.env.NODE_ENV !== 'production' && typeof window !== "undefined")
 const port = process.env.REDUX_REMOTE_PORT || 8000
+if (isDev) {
+    middlewares.push(createLogger())
+}
 
-let composeEnhancers = composeWithDevTools({ realtime: !isDev })
+let composeEnhancers = composeWithDevTools({ realtime: !isDev });
 
 if (typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
     composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
 }
 
+export const configureStore = (reducer, preloadState) => {
+    return createStore(reducer, preloadState, composeEnhancers(applyMiddleware(...middlewares)))
+}
 
-export const configureStore = compose(isDev ? composeEnhancers(applyMiddleware(...middlewares)) :
-    applyMiddleware(...middlewares))(createStore)
-
-export function connected(actions = {}, mapStateToProps = state => state) {
-    return OriginalComponent => {
+export function connected(mapStateToProps = state => state, actions = {}) {
+    return Component => {
         const mapActionToProps = dispatch => ({
             dispatch,
             actions: bindActionCreators(actions, dispatch)
         })
-        return connect(mapStateToProps, mapActionToProps)(OriginalComponent)
+        return connect(mapStateToProps, mapActionToProps)(Component)
     }
 }
 
 export function wrapper(store) {
-    return OriginalComponent => {
+    return Component => {
         return class extends React.Component {
             render() {
                 return (
                     <Provider store={store}>
-                    <OriginalComponent {...this.props}/>
+                    <Component {...this.props}/>
                     </Provider>
                 )
             }
@@ -45,9 +49,9 @@ export function wrapper(store) {
 }
 
 export default function(rootReducer, initialState = {}, actions = {}, mapStateToProps = state => state) {
-    return OriginalComponent => {
+    return Component => {
         const store = configureStore(rootReducer, initialState)
-        const ConnectedComponent = connected(actions, mapStateToProps)(OriginalComponent)
+        const ConnectedComponent = connected(mapStateToProps, actions)(Component)
         return wrapper(store)(ConnectedComponent)
     }
 }
